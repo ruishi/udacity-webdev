c150f4f015e5326da34b92bc9431ff543074c89c
ruishi <ganruishi@gmail.com>
2014-03-20 17:11:29 -0400

    add basic wiki registration
    Signup, login, and logout functionality added to wiki.

--- routes.py
+++ routes.py
@@ -6,18 +6,28 @@
 from webapp2 import WSGIApplication
 
 import rot13.rot13 as rot13
-import blog.register as r
+import blog.register as br
 import blog.blog as b
+import wiki.register as wr
+import wiki.wiki as w
 import main
 
 app = WSGIApplication([('/', main.MainPage)], debug=True)
+
 r13app = WSGIApplication([(r'/rot13', rot13.Rot13)], debug=True)
-blogapp = WSGIApplication([(r'/blog/welcome', r.Welcome),
-                           (r'/blog/signup', r.Register),
-                           (r'/blog/login', r.Login),
-                           (r'/blog/logout', r.Logout),
+
+blogapp = WSGIApplication([(r'/blog/welcome', br.Welcome),
+                           (r'/blog/signup', br.Register),
+                           (r'/blog/login', br.Login),
+                           (r'/blog/logout', br.Logout),
                            (r'/blog/?', b.Blog),
                            (r'/blog/newpost', b.WritePost),
                            (r'/blog/(\d+)', b.Permalink),
                            (r'/blog/(\d+)\.json|/blog/\.json', b.JSONHandler),
                            (r'/blog/flush/?', b.FlushCache)], debug=True)
+
+wikiapp = WSGIApplication([(r'/wiki/?', w.Front),
+                           (r'/wiki/welcome', wr.Welcome),
+                           (r'/wiki/signup', wr.Register),
+                           (r'/wiki/login', wr.Login),
+                           (r'/wiki/logout', wr.Logout)], debug=True)
